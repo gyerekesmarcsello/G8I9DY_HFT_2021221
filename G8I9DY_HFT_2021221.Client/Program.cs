289831559fef296e83d96456c1de72925348c484
marce <marce@DESKTOP-GL2FB2E>
2021-10-08 20:10:27 +0200

    Add project files.

---
+++ G8I9DY_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace G8I9DY_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
