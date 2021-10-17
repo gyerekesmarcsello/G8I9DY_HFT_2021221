497f98cdb3b9a005f1ed7e5ed01459c4aba49825
marcell.lenkei@stud.uni-obuda.hu <marcell.lenkei@stud.uni-obuda.hu>
2021-10-17 15:28:06 +0200

    xDbContext created

--- G8I9DY_HFT_2021221.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace G8I9DY_HFT_2021221.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ G8I9DY_HFT_2021221.Data/TracksDbContext.cs
@@ -0,0 +1,36 @@
+﻿using G8I9DY_HFT_2021221.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G8I9DY_HFT_2021221.Data
+{
+    class TracksDbContext : DbContext
+    {
+        public virtual DbSet<Albums> Albums { get; set; }
+
+        public virtual DbSet<Artists> Artist { get; set; }
+
+        public virtual DbSet<Tracks> Tracks { get; set; }
+
+        public TracksDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\LocalDB.mdf;Integrated Security=True;MultipleActiveResultSets=True");
+            }
+        }
+
+
+    }
+}
