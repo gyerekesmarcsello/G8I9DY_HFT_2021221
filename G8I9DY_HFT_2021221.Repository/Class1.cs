66c59c0978a53fd747eda88a578f623c96ab2359
marcell.lenkei@stud.uni-obuda.hu <marcell.lenkei@stud.uni-obuda.hu>
2021-11-14 19:01:49 +0100

    Repository classes created

--- G8I9DY_HFT_2021221.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace G8I9DY_HFT_2021221.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ G8I9DY_HFT_2021221.Repository/TrackRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G8I9DY_HFT_2021221.Repository
+{
+    class TrackRepository
+    {
+    }
+}
