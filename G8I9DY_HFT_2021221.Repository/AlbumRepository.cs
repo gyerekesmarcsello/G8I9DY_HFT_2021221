ffc98979fdf1e535b50bc407608912997940d9cc
marcell.lenkei@stud.uni-obuda.hu <marcell.lenkei@stud.uni-obuda.hu>
2021-11-14 19:32:53 +0100

    Repository class created

--- G8I9DY_HFT_2021221.Repository/AlbumRepository.cs
+++ G8I9DY_HFT_2021221.Repository/AlbumRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using G8I9DY_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,41 @@ using System.Threading.Tasks;
 
 namespace G8I9DY_HFT_2021221.Repository
 {
-    class AlbumRepository
+    class AlbumRepository : IRepository<Albums>, IAlbumRepository
     {
+        public void CreateAlbum(int albumID, string Title, int ArtistID, string Label)
+        {
+            Albums album = new Albums() { AlbumID = albumID, Title = Title, ArtistID = ArtistID, Label = Label };
+        }
+
+        public void DeleteAlbum(int albumID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Albums> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Albums GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Albums ReadAlbum(int albumID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HashSet<Albums> ReadAllAlbums()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAlbum(int albumID, string Title, int ArtistID, string Label)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ G8I9DY_HFT_2021221.Repository/Repository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G8I9DY_HFT_2021221.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected DbContext context;
+        public Repository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return context.Set<T>();
+        }
+        public void Create(T entity)
+        {
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+        }
+        public void Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            context.SaveChanges();
+        }
+        public abstract T GetOne(int id);
+    }
+}
