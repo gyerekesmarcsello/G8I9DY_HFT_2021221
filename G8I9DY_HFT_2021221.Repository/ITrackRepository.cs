9c2eaf6832d1087563455985885f7b50e3b64892
marcell.lenkei@stud.uni-obuda.hu <marcell.lenkei@stud.uni-obuda.hu>
2021-11-14 19:22:51 +0100

    Track repository created

---
+++ G8I9DY_HFT_2021221.Repository/ITrackRepository.cs
@@ -0,0 +1,23 @@
+﻿using G8I9DY_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G8I9DY_HFT_2021221.Repository
+{
+    public interface ITrackRepository: IRepository<Tracks>
+    {
+        //create
+        void CreateTrack(int TrackID, string Title, int AlbumID, string Genre, int ArtistID, string ReleaseDate);
+        //read
+        Albums ReadTrack(int TrackID);
+        //readall
+        HashSet<Artists> ReadAllTracks();
+        //update
+        void UpdateTrack(int TrackID, string Title, int AlbumID, string Genre, int ArtistID, string ReleaseDate);
+        //delete
+        void DeleteTrack(int TrackID);
+    }
+}
