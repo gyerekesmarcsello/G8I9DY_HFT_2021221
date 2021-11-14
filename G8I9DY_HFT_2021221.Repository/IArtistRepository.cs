fb18a76453c368cf2c682ccfef1508066752afc2
marcell.lenkei@stud.uni-obuda.hu <marcell.lenkei@stud.uni-obuda.hu>
2021-11-14 19:15:16 +0100

    Album,Artist interface created

---
+++ G8I9DY_HFT_2021221.Repository/IArtistRepository.cs
@@ -0,0 +1,23 @@
+﻿using G8I9DY_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G8I9DY_HFT_2021221.Repository
+{
+    public interface IArtistRepository : IRepository<Artists>
+    {
+        //create
+        void CreateAlbum(int ArtistID, string Name, string Birthday, string Country);
+        //read
+        Albums ReadAlbum(int ArtistID);
+        //readall
+        HashSet<Albums> ReadAllAlbums();
+        //update
+        void UpdateAlbum(int ArtistID, string Name, string Birthday, string Country);
+        //delete
+        void DeleteAlbum(int ArtistID);
+    }
+}
