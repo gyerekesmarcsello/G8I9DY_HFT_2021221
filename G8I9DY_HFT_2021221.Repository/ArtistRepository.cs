d47318ff8b331571e690e95511c337d0ab282aa0
marcell.lenkei@stud.uni-obuda.hu <marcell.lenkei@stud.uni-obuda.hu>
2021-11-14 19:56:22 +0100

    ArtistRepo filled

--- G8I9DY_HFT_2021221.Repository/ArtistRepository.cs
+++ G8I9DY_HFT_2021221.Repository/ArtistRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using G8I9DY_HFT_2021221.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,44 @@ using System.Threading.Tasks;
 
 namespace G8I9DY_HFT_2021221.Repository
 {
-    class ArtistRepository
+    class ArtistRepository : Repository<Artists>, IArtistRepository
     {
+        public ArtistRepository(DbContext context) : base(context)
+        {
+
+        }
+        public void CreateArtist(int ArtistID, string Name, string Birthday, string Country)
+        {
+            Artists artist = new Artists() { ArtistID = ArtistID, Name = Name, Birthday = Birthday, Country = Country };
+            context.SaveChanges();
+        }
+
+        public void DeleteArtist(int ArtistID)
+        {
+            Delete(GetOne(ArtistID));
+            context.SaveChanges();
+        }
+
+        public override Artists GetOne(int id)
+        {
+            return GetAll().SingleOrDefault(x => x.ArtistID == id);
+        }
+
+        public HashSet<Artists> ReadAllArtist()
+        {
+            return (HashSet<Artists>)GetAll();
+        }
+
+        public Artists ReadArtist(int ArtistID)
+        {
+            return GetOne(ArtistID);
+        }
+
+        public void UpdateArtist(int ArtistID, string Name, string Birthday, string Country)
+        {
+            DeleteArtist(ArtistID);
+            CreateArtist(ArtistID, Name, Birthday, Country);
+            context.SaveChanges();
+        }
     }
 }
--- G8I9DY_HFT_2021221.Repository/IArtistRepository.cs
+++ G8I9DY_HFT_2021221.Repository/IArtistRepository.cs
@@ -12,7 +12,7 @@ namespace G8I9DY_HFT_2021221.Repository
         //create
         void CreateArtist(int ArtistID, string Name, string Birthday, string Country);
         //read
-        Albums ReadArtist(int ArtistID);
+        Artists ReadArtist(int ArtistID);
         //readall
         HashSet<Artists> ReadAllArtist();
         //update
