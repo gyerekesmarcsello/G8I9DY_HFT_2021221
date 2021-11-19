9a7263f0b257e889dd3488f1742adb69275d1531
Marcell Lenkei <marcell.lenkei@gmail.com>
2021-11-19 20:06:07 +0100

    trackLogic filled

--- G8I9DY_HFT_2021221.Logic/TrackLogic.cs
+++ G8I9DY_HFT_2021221.Logic/TrackLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using G8I9DY_HFT_2021221.Models;
+using G8I9DY_HFT_2021221.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,39 @@ using System.Threading.Tasks;
 
 namespace G8I9DY_HFT_2021221.Logic
 {
-    public class TrackLogic
+    public class TrackLogic : ITrackLogic
     {
+        ITrackRepository trackRepo;
 
+        public TrackLogic(ITrackRepository trackRepo)
+        {
+            this.trackRepo = trackRepo;
+        }
+
+        public void CreateTrack(int TrackID, string Title, int AlbumID, string Genre, int ArtistID, string ReleaseDate)
+        {
+            trackRepo.CreateTrack(TrackID, Title, AlbumID, Genre, ArtistID, ReleaseDate);   
+            
+        }
+
+        public void DeleteTrack(int TrackID)
+        {
+            trackRepo.DeleteTrack(TrackID);
+        }
+
+        public IEnumerable<Tracks> ReadAllTracks()
+        {
+            return trackRepo.ReadAllTracks();
+        }
+
+        public Tracks ReadTrack(int TrackID)
+        {
+            return trackRepo.ReadTrack(TrackID);
+        }
+
+        public void UpdateTrack(int TrackID, string Title, int AlbumID, string Genre, int ArtistID, string ReleaseDate)
+        {
+            trackRepo.UpdateTrack(TrackID, Title, AlbumID, Genre, ArtistID, ReleaseDate);
+        }
     }
 }
