79adf8b2cc1e763380b3ba04071c9557c65a39bf
Marcell Lenkei <marcell.lenkei@gmail.com>
2021-11-19 20:01:25 +0100

    artistLogic filled

--- G8I9DY_HFT_2021221.Logic/ArtistLogic.cs
+++ G8I9DY_HFT_2021221.Logic/ArtistLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using G8I9DY_HFT_2021221.Models;
+using G8I9DY_HFT_2021221.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,37 @@ using System.Threading.Tasks;
 
 namespace G8I9DY_HFT_2021221.Logic
 {
-    public class ArtistLogic
+    public class ArtistLogic : IArtistLogic
     {
+        IArtistRepository artistRepo;
+        public ArtistLogic(IArtistRepository artistRepo)
+        {
+            this.artistRepo = artistRepo;
+        }
 
+        public void CreateArtist(int ArtistID, string Name, string Birthday, string Country)
+        {
+           artistRepo.CreateArtist(ArtistID, Name, Birthday, Country);  
+        }
+
+        public void DeleteArtist(int ArtistID)
+        {
+            artistRepo.DeleteArtist(ArtistID);
+        }
+
+        public IEnumerable<Artists> ReadAllArtist()
+        {
+            return artistRepo.ReadAllArtist();
+        }
+
+        public Artists ReadArtist(int ArtistID)
+        {
+            return artistRepo.ReadArtist(ArtistID);
+        }
+
+        public void UpdateArtist(int ArtistID, string Name, string Birthday, string Country)
+        {
+            artistRepo.UpdateArtist(ArtistID, Name, Birthday, Country);
+        }
     }
 }
