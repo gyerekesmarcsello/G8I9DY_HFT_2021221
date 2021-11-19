cd0064be3e0f81d6c9569282998d2458d395c18b
Marcell Lenkei <marcell.lenkei@gmail.com>
2021-11-19 19:50:24 +0100

    unnecessary things removed

--- G8I9DY_HFT_2021221.Logic/AlbumLogic.cs
+++ G8I9DY_HFT_2021221.Logic/AlbumLogic.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using G8I9DY_HFT_2021221.Models;
+using G8I9DY_HFT_2021221.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,46 @@ using System.Threading.Tasks;
 
 namespace G8I9DY_HFT_2021221.Logic
 {
-    public class AlbumLogic
+    public class AlbumLogic : IAlbumLogic
     {
+        IAlbumRepository albumRepo;
+        public AlbumLogic(IAlbumRepository albumRepo)
+        {
+            this.albumRepo = albumRepo;
+        }
+        public void CreateAlbum(int albumID, string Title, int ArtistID, string Label)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void DeleteAlbum(int albumID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Albums> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Albums GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Albums ReadAlbum(int albumID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Albums> ReadAllAlbums()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAlbum(int albumID, string Title, int ArtistID, string Label)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
