f8aebb5522ad46b08bb93fdb55a2ab999c56741c
Marcell Lenkei <marcell.lenkei@gmail.com>
2021-11-26 18:05:46 +0100

    Controller classes created

---
+++ G8I9DY_HFT_2021221.Endpoint/AlbumController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G8I9DY_HFT_2021221.Endpoint
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+    }
+}
