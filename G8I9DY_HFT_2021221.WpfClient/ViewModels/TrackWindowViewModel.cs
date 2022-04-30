1c2dd5cb210ce70d6d7d19e61e08301de94eb023
Marcell Lenkei <marcell.lenkei@gmail.com>
2022-04-30 18:13:25 +0200

    done (?)

--- G8I9DY_HFT_2021221.WpfClient/ViewModels/TrackWindowViewModel.cs
+++ G8I9DY_HFT_2021221.WpfClient/ViewModels/TrackWindowViewModel.cs
@@ -12,7 +12,105 @@ using System.Windows.Input;
 
 namespace G8I9DY_HFT_2021221.WpfClient.ViewModels
 {
-    public class TrackWindowViewModel
+    public class TrackWindowViewModel:ObservableRecipient
     {
+        private string errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set { SetProperty(ref errorMessage, value); }
+        }
+
+        public RestCollection<Tracks> Tracks { get; set; }
+
+
+        private Tracks selectedTrack;
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
+        public Tracks SelectedTrack
+        {
+            get { return selectedTrack; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedTrack = new Tracks()
+                    {
+                        TrackID = value.TrackID,
+                        Title = value.Title,
+                        AlbumID = value.AlbumID,
+                        Plays = value.Plays,
+                        Duration = value.Duration,
+                        IsExplicit = value.IsExplicit,
+                        ArtistID = value.ArtistID,
+
+                    };
+
+                    OnPropertyChanged();
+                    (DeleteTrackCommand as RelayCommand).NotifyCanExecuteChanged();
+                    (UpdateTrackCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+        public ICommand CreateTrackCommand { get; set; }
+
+        public ICommand DeleteTrackCommand { get; set; }
+
+        public ICommand UpdateTrackCommand { get; set; }
+
+        public TrackWindowViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                Tracks = new RestCollection<Tracks>("http://localhost:57125/", "tracks", "hub");
+                CreateTrackCommand = new RelayCommand(() =>
+                {
+                    Tracks.Add(new Tracks()
+                    {
+                        Title = selectedTrack.Title,
+                        AlbumID = selectedTrack.AlbumID,
+                        Plays = selectedTrack.Plays,
+                        Duration = selectedTrack.Duration,
+                        IsExplicit= selectedTrack.IsExplicit,
+                        ArtistID = selectedTrack.ArtistID,
+                    });
+                });
+
+                UpdateTrackCommand = new RelayCommand(() =>
+                {
+                    try
+                    {
+                        Tracks.Update(selectedTrack);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        ErrorMessage = ex.Message;
+                    }
+
+                });
+
+                DeleteTrackCommand = new RelayCommand(() =>
+                {
+                    Tracks.Delete(selectedTrack.TrackID);
+                },
+                () =>
+                {
+                    return selectedTrack != null;
+                });
+                selectedTrack = new Tracks();
+            }
+
+        }
     }
 }
+
